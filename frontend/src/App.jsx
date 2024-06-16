9805608962c54f52b8325084bb3044d7c5f0be35
aymanaarab <soquavo@gmail.com>
2024-06-16 20:44:43 +0000

    !

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,14 @@
-import { useState } from 'react'
 import './App.css'
 import FileUpload from './components/FileUpload'
 
 function App() {
 
   return (
-<<<<<<< HEAD
     <>
      
 
       <FileUpload/>
     </>
-=======
-    <div className="container">
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/books" element={<Books />} />
-      <Route path="/cart" element={<Cart />} />
-      <Route path="/profile" element={<Profile />} />
-      <Route path="/auth/:type" element={<Auth />} />
-    </Routes>
-  </div>
->>>>>>> 6a560b8fec01352d6acdf5d52713f95e44bac1c5
   )
 }
 
